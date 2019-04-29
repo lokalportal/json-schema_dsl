e5ee53c0153a859968214822cb7d507efd58df8f
Paul Martensen <paul.martensen@gmx.de>
2019-04-29 12:41:31 +0200

    Simplified monad implementation.

--- lib/json/schema_dsl/monads.rb
+++ lib/json/schema_dsl/monads.rb
@@ -6,39 +6,25 @@ module JSON
           attr_accessor :identifier_key
         end
 
+        attr_reader :inner
+
         def initialize(inner)
           @inner = inner
-          @applicable = dig_inner[self.class.identifier_key].present?
+          @applicable = inner[self.class.identifier_key].present?
         end
 
-        def dig_inner
-          if @inner.is_a?(::JSON::SchemaDsl::Monads::Base)
-            @inner.dig_inner
-          else
-            @inner
-          end
-        end
-
-        def fmap(&block)
-          @inner = if @inner.is_a?(Base)
-                     @inner.fmap(&block)
-                   else
-                     yield @inner
-                   end
+        def fmap
+          @inner = yield @inner
           self
         end
 
         def as_json
-          if @applicable
-            monad_json
-          else
-            @inner.as_json
-          end
+          @applicable ? monad_json : @inner.as_json
         end
 
         def monad_json
           key = self.class.identifier_key.to_s.camelize(:lower)
-          { key => dig_inner[key].map { |ch| Renderer.render(ch) } }.as_json
+          { key => inner[key].map { |ch| Renderer.render(ch) } }.as_json
         end
       end
 
