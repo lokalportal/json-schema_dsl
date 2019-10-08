ebe5084e8d91acec2f6ece53b436e9267f09020b
Paul Martensen <paul.martensen@gmx.de>
2019-10-08 11:45:15 +0200

    Added container removal.
    A container in that sense is an entity that only has a type and an
    `any_of` (or other collection) field. It does not make sense to wrap
    them so they are left out of the new resulting collection.

--- lib/json/schema_dsl/renderers/multiplexer.rb
+++ lib/json/schema_dsl/renderers/multiplexer.rb
@@ -17,10 +17,16 @@ module JSON
             present_key = entity[:type] != 'entity' && KEYS.find { |k| entity[k].present? }
             return entity unless present_key
 
-            {
-              type: 'entity',
-              present_key => entity[present_key].map { |ch| visit(ch) }
-            }
+            new_value = entity[present_key].map { |ch| visit(ch) }
+            new_value.push(entity.except(present_key)) unless container?(entity.except(present_key))
+
+            { type: 'entity', present_key => new_value.reject(&:blank?) }
+          end
+
+          def container?(entity)
+            cleaned_up = Renderers::Filter.visit(entity)
+
+            cleaned_up[:type].to_s == 'object' && cleaned_up.keys.count == 1
           end
         end
       end
