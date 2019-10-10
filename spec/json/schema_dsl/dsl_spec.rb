10b702e8e715cf563d5b109ddc80a1b1972794f3
Paul Martensen <paul.martensen@gmx.de>
2019-10-10 07:46:36 +0200

    Made the helper method example more realistic.

--- spec/json/schema_dsl/dsl_spec.rb
+++ spec/json/schema_dsl/dsl_spec.rb
@@ -1,23 +1,28 @@
 # frozen_string_literal: true
 
 describe JSON::SchemaDsl do
-  include JSON::SchemaDsl
-
   describe 'context helper methods' do
-    let(:tree) do
-      array do
-        items do
-          any_of james
+    let(:dummy) do
+      Class.new do
+        include JSON::SchemaDsl
+
+        def tree
+          object do
+            string :jeff
+            object :data do
+              helper
+            end
+          end
+        end
+
+        def helper
+          object(:helper)
         end
       end
     end
 
-    def james
-      [object, string]
-    end
-
     it 'is able to use the instance methods' do
-      expect { tree }.not_to raise_error
+      expect { dummy.new.tree }.not_to raise_error
     end
   end
 end
