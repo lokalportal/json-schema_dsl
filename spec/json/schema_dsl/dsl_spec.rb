eb7d0e52800490c07799e7e52fd1edabf4278340
Paul Martensen <paul.martensen@gmx.de>
2019-10-10 08:14:25 +0200

    Added test to inspect generated structure.

--- spec/json/schema_dsl/dsl_spec.rb
+++ spec/json/schema_dsl/dsl_spec.rb
@@ -21,8 +21,29 @@ describe JSON::SchemaDsl do
       end
     end
 
+    let(:expected) do
+      {
+        type: :object,
+        properties: {
+          jeff: { type: :string },
+          data: {
+            type: :object,
+            properties: {
+              helper: {
+                type: :object
+              }
+            }
+          }
+        }
+      }
+    end
+
     it 'is able to use the instance methods' do
       expect { dummy.new.tree }.not_to raise_error
+    end
+
+    it 'is able not creating unnecessary children but using the return value' do
+      expect(dummy.new.tree.as_json).to eq(expected.as_json)
     end
   end
 end
