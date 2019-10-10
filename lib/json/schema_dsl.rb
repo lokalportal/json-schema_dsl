c5194f20f187908fe17c424a3c52e50794afced5
Paul Martensen <paul.martensen@gmx.de>
2019-10-10 12:19:08 +0200

    Added modular types and type extensions.

--- lib/json/schema_dsl.rb
+++ lib/json/schema_dsl.rb
@@ -8,6 +8,7 @@ require 'dry-types'
 
 require 'json/schema_dsl/version'
 require 'json/schema_dsl/types'
+require 'json/schema_dsl/configuration'
 require 'json/schema_dsl/entity'
 
 %w[null boolean numeric integer string object array].each do |type|
@@ -22,9 +23,45 @@ module JSON
   module SchemaDsl
     class Error < StandardError; end
 
-    delegate(:type_defaults,
-             :reset_type_defaults!,
-             :add_defaults_for,
-             to: ::JSON::SchemaDsl::Builder)
+    class << self
+      delegate(:type_defaults,
+               :reset_type_defaults!,
+               :add_defaults_for,
+               to: ::JSON::SchemaDsl::Builder)
+
+      DEFAULT_TYPES = JSON::SchemaDsl::Entity.descendants.dup.push(JSON::SchemaDsl::Entity).freeze
+
+      def registered_types
+        @registered_types ||= DEFAULT_TYPES.dup
+      end
+
+      def register_type(type)
+        registered_types.push(type).tap { define_type_method(type) }
+      end
+
+      def reset_schema_dsl!
+        (registered_types.map { |t| type_method_name(t).to_sym } & instance_methods)
+          .each { |tm| remove_method tm }
+        @registered_types = DEFAULT_TYPES.dup
+        define_schema_dsl!
+      end
+
+      def define_schema_dsl!
+        registered_types.map { |t| define_type_method(t) }
+      end
+
+      def define_type_method(type)
+        builder = JSON::SchemaDsl::Builder[type]
+        define_method(type_method_name(type)) do |name = nil, **attributes, &block|
+          builder.build(name, **attributes, scope: self, &block)
+        end
+      end
+
+      def type_method_name(type)
+        type.type_method_name || 'entity'
+      end
+    end
+
+    define_schema_dsl!
   end
 end
--- spec/json/schema_dsl_spec.rb
+++ spec/json/schema_dsl_spec.rb
@@ -1,5 +1,64 @@
 # frozen_string_literal: true
 
-describe Json::SchemaDsl do
-  include JSON::SchemaDsl
+describe JSON::SchemaDsl do
+  include described_class
+  after { described_class.reset_schema_dsl! }
+
+  describe '#register_type' do
+    let!(:email_type) do
+      Class.new(JSON::SchemaDsl::String) do
+        def self.type_method_name
+          'email'
+        end
+
+        def self.infer_type
+          'string'
+        end
+
+        attribute(:format, JSON::SchemaDsl::Types::String.default('e-mail'))
+      end
+    end
+
+    shared_context('when type registered') do
+      before { described_class.register_type(email_type) }
+    end
+
+    context 'when the type has not been registered' do
+      subject { described_class.instance_methods }
+
+      it { is_expected.not_to include(:email) }
+    end
+
+    context 'when the type has been registered' do
+      include_context 'when type registered'
+
+      it 'defines the type method' do
+        expect(described_class.instance_methods).to include(:email)
+      end
+
+      it 'is added to registered types' do
+        expect(described_class.registered_types).to include(email_type)
+      end
+
+      it 'can be undefined using reset' do
+        described_class.reset_schema_dsl!
+        expect(described_class.instance_methods).not_to include(:email)
+      end
+    end
+
+    context 'when the new type is used in a schema' do
+      include_context 'when type registered'
+
+      let(:schema) do
+        object do
+          email :email_address
+        end
+      end
+
+      it 'inherits the extended type' do
+        expect(schema.as_json.dig('properties', 'email_address'))
+          .to eq({ type: 'string', format: 'e-mail' }.as_json)
+      end
+    end
+  end
 end
